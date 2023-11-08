e147ce28feb3566d32b1b80706150e38d902badc
Alfredo Emir Puente Medrano <54431930+I-EmiR-I@users.noreply.github.com>
2023-11-08 03:43:51 -0600

    Add files via upload

---
+++ DB/indexMovimiento.php
@@ -0,0 +1,24 @@
+<?php
+	$Datos = $_GET["Datos"];
+	//$Datos2 = $_GET["Datos2"];
+
+	$usuario = "root";
+	$contrasena = "";
+	$servidor = "localhost";
+	$basededatos = "basededatos";
+
+	$conexion = mysqli_connect( $servidor, $usuario, "" ) or die ("No se ha podido conectar al servidor de base de datos");
+
+	$db = mysqli_select_db( $conexion, $basededatos ) or die ("No se ha podido seleccionar la base de datos");
+
+	$fecha = date("Y-m-d H:i:s");  // Get the current date and time in the format "YYYY-MM-DD HH:MM:SS"
+
+    $consulta = "INSERT INTO sensoresmovimiento (Movimiento, Fecha) VALUES (" . $Datos . ", '" . $fecha . "')";
+	
+	$resultado = mysqli_query( $conexion, $consulta );
+	if ($resultado) {
+		echo "Datos insertados correctamente.";
+	} else {
+		echo "Error al insertar datos: " . mysqli_error($conexion);
+	}
+?>
