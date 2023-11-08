e217a2252202c91412d9c7577a9e3a9aa8ffd4d0
Alfredo Emir Puente Medrano <54431930+I-EmiR-I@users.noreply.github.com>
2023-11-08 03:42:08 -0600

    Update GPS_Script.ino

--- GPS_Script.ino
+++ GPS_Script.ino
@@ -6,10 +6,19 @@
 static const int RX = D6, TX = D7;
 static const uint32_t GPSBaud = 9600;
 
-const char* ssid = "wifi name";  // SSID of your WiFi
-const char* password = "password";  // Password of your WiFi
-unsigned long ch_no = 11233123;  // Replace with Thingspeak Channel number
+const char* ssid = "";  // SSID of your WiFi
+const char* password = "";  // Password of your WiFi
+unsigned long ch_no = 151231;  // Replace with Thingspeak Channel number
 const char* write_api = "API KEY";  // Replace with Thingspeak write API
+
+//db connection
+const char* host = "192.168.1.8";// IP server, obtained with 'ipconfig' in command shell
+const int port = 80;// server port, configured in xampp for Apache server, default is: 80
+
+const int watchdog = 5000;// watchdog frequency, reconnect timeout
+unsigned long previousMillis = millis();
+int c = 0;// the data
+
 
 TinyGPSPlus gps;
 WiFiClient client;
@@ -36,6 +45,7 @@ void setup() {
 }
 
 void loop() {
+  
   while (soft.available() > 0)
     if (gps.encode(soft.read())) {
       displaydata();
@@ -57,6 +67,52 @@ void displaydata() {
     ThingSpeak.setField(1, latitude_data);
     ThingSpeak.setField(2, longitude_data);
     ThingSpeak.writeFields(ch_no, write_api);
+    
+    //send data to database
+    unsigned long currentMillis = millis();
+  if(currentMillis - previousMillis > watchdog){
+    previousMillis = currentMillis;
+    WiFiClient client;// creating the client
+
+    // checking if we can connect to the server
+    if(!client.connect(host, port)){
+      Serial.println("Fail to connect");
+      return;
+    }
+
+    // Convertir las variables float a cadenas (String)
+    
+    String strVariable1 = String(latitude); // 2 indica el número de decimales
+    String strVariable2 = String(longitude); // 2 indica el número de decimales
+    
+    Serial.println(strVariable1);
+    // building our url according our database
+    String url = "http://localhost/DB/indexGPS.php?Datos1=" + strVariable1 + "&Datos2=" + strVariable2;
+
+    url += c;
+
+    //Print data to the server that a client is connected to
+    client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+                "Host: " + host + "\r\n" +
+                "Connection: close\r\n\r\n");
+
+    // if watchdog timeout, the client is stopped
+    unsigned long timeout = millis();
+    while(client.available() == 0){
+      if(millis() - timeout > 5000){
+        Serial.println(">>> Client Timeout !");
+        client.stop();
+        return;
+      }
+    }
+
+    //Read reply from the server
+    while(client.available()){
+      String line = client.readStringUntil('\r');
+      Serial.print(line);
+    }
+  }
+
     delay(20000);
   } else {
     Serial.println("Data error!!!");
